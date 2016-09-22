f5a2b32182eccb256c97ed8b51d88d84aff03eb4
zachpeterw <zwhi8516@740s-pl-129.local>
2016-09-22 13:29:12 -0600

    added more loops(for practice)

--- src/loops/controller/LoopsController.java
+++ src/loops/controller/LoopsController.java
@@ -3,4 +3,40 @@ package loops.controller;
 public class LoopsController
 {
 
+	public void start()
+	{
+		System.out.println("loops practice started");
+		loopMethod();
+	}
+	private void loopMethod()
+	{
+		int numberOfTimes = 0;
+		while (numberOfTimes < 20)
+		{
+			System.out.println("hello");
+			numberOfTimes++;
+		}
+		int cats = 0;
+		while (cats < 3)
+		{
+			System.out.println("meow!");
+			cats++;
+		}
+		for (int dogs = 0; dogs < 20; dogs++)
+		{
+			System.out.println("bork!");
+		}
+		for (int cows = 0; cows < 10; cows++)
+		{
+			System.out.println("moooooooo!!!");
+		}
+	
+	
+	}
+	
+	
+	
+	
+
 }
+ 
--- src/loops/controller/LoopsRunner.java
+++ src/loops/controller/LoopsRunner.java
@@ -1,6 +1,10 @@
 package loops.controller;
 
-public class LoopsRunner
-{
-
+public class LoopsRunner {
+	public static void main (String [ ] args) 
+	{
+		LoopsController loopsAppController = new LoopsController();
+		loopsAppController.start();
+	}
+	
 }
